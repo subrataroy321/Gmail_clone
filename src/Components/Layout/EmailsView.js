d540f6f8b1270b843efcf131e6083edf3688bf10
SUBRATA ROY <subrata.r321@gmail.com>
2021-05-08 14:21:37 -0400

    Add icons to email filter at top

--- src/Components/Layout/EmailsView.js
+++ src/Components/Layout/EmailsView.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import CheckBox from '@material-ui/core/CheckBox';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 
 function EmailsView() {
     return (
         <Wrapper>
-            <p>Email View</p>
+            <TopWrapper>
+                <TopLeft>
+                    <CheckBox />
+                    <IconButton>
+                        <RefreshIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </TopLeft>
+                <TopRight>
+                    <IconButton>
+                        <KeyboardArrowRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardArrowLeftIcon/>
+                    </IconButton>
+                </TopRight>
+
+            </TopWrapper>
+            <BodyWrapper>
+
+            </BodyWrapper>
         </Wrapper>
     )
 }
@@ -13,3 +41,17 @@ function EmailsView() {
 export default EmailsView
 
 const Wrapper = styled.div``
+const TopWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 50% 50%;
+    border-bottom: 1px solid lightgray;
+
+`
+const BodyWrapper = styled.div``
+const TopLeft = styled.div`
+    margin-left: 15px;
+`
+const TopRight = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+`
